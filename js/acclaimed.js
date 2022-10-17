66e1102f2eb6a719d075d7e0984fc680e5b71c99
galeze <ezequiel.p.galeano@gmail.com>
2022-10-17 00:51:10 -0300

    Se reemplaza URL por const

--- js/acclaimed.js
+++ js/acclaimed.js
@@ -1,23 +1,16 @@
-
 async function showMostAcclaimedMovies() {
-    const container = document.getElementById("acclaimed-container");
-  const datos = await getJsonFromAPI("https://api.themoviedb.org/3/movie/top_rated?api_key=1cf50e6248dc270629e802686245c2c8&language=es-AR&page=1");
+  const container = document.getElementById("acclaimed-container");
+  const datos = await getJsonFromAPI(TOP_RATED_API_URL);
   datos.results.forEach((movie) => {
     const { title, poster_path, vote_average, overview, id } = movie;
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
-         <img src="${
-           poster_path
-             ? IMG_URL + poster_path
-             : "http://via.placeholder.com/1080x1580"
-         }" alt="${title}">
-
+        <img src="${IMG_URL + poster_path}" alt="${title}">
         <div class="movie-info">
             <h3>${title}</h3>
             <span class="${getColor(vote_average)}">${vote_average}</span>
         </div>
-
         <div class="overview">
 
             <h3>Overview</h3>
@@ -25,26 +18,19 @@ async function showMostAcclaimedMovies() {
             <br/> 
             <button class="know-more" id="${id}"><i class="fas fa-info-circle"></i>+Info</button>
         </div>
-    
     `;
-
     container.appendChild(movieEl);
-
-    document.getElementById(id).addEventListener("click", () => {
-      console.log(id);
-      openNav(movie);
-    });
   });
 }
 
 showMostAcclaimedMovies();
 
 function getColor(vote) {
-    if(vote>= 8){
-        return 'green'
-    }else if(vote >= 5){
-        return "orange"
-    }else{
-        return 'red'
-    }
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const API_KEY = "api_key=1cf50e6248dc270629e802686245c2c8";
-const BASE_URL = "https://api.themoviedb.org/3";
-const TRENDING_WEEK_API_URL = BASE_URL + "/trending/movie/week?" + API_KEY + "&language=es-AR";
-const POPULARITY_API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY + "&language=es-AR";
-const TOP_RATED_API_URL = BASE_URL + "/discover/movie?sort_by=vote_average.desc&" + API_KEY + "&language=es-AR";
+const LANG = "&language=es-AR";
+const BASE_URL = "https://api.themoviedb.org/3/";
+const TRENDING_WEEK_API_URL = BASE_URL + "trending/movie/week?" + API_KEY + "&language=es-AR";
+const POPULARITY_API_URL = BASE_URL + "discover/movie?sort_by=popularity.desc&" + API_KEY + "&language=es-AR";
+const TOP_RATED_API_URL = BASE_URL +"movie/top_rated?" + API_KEY + LANG + "&page=1";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+
 
 
 
